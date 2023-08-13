03f15f2c40d877d5895ddfe61dd438bf98139f5f
rahul <vekariyarahul661@gmail.com>
2023-08-13 15:46:47 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from "./reducers/store";
 import { Provider } from "react-redux";
 
 store.subscribe(() => console.log(store.getState()));
-
+ 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
